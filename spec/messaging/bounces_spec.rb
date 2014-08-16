ab7dc3c8d50f806be8b6fd20be102660a98b79f1
Sebastian Spier <sebastian.spier@meltwater.com>
2014-08-16 17:32:35 +0200

    fixes tests for bounces

--- spec/messaging/bounces_spec.rb
+++ spec/messaging/bounces_spec.rb
@@ -41,7 +41,7 @@ describe Dyn::Messaging::Client do
         start_time = 1
         end_time = 2
 
-        stub = stub_request(:get, "#{@API_BASE_PATH}/reports/bounces?apikey=#{@DEFAULT_API_KEY}&starttime=#{start_time}&endtime=#{end_time}&startindex=0")
+        stub = stub_request(:get, "#{@API_BASE_PATH}/reports/bounces?apikey=#{@DEFAULT_API_KEY}&starttime=#{start_time}&endtime=#{end_time}&startindex=0&emailaddress=")
 
         @dyn.bounces.list(start_time,end_time)
 
@@ -53,9 +53,22 @@ describe Dyn::Messaging::Client do
         end_time = 2
         start_index = 40
 
-        stub = stub_request(:get, "#{@API_BASE_PATH}/reports/bounces?apikey=#{@DEFAULT_API_KEY}&starttime=#{start_time}&endtime=#{end_time}&startindex=#{start_index}")
+        stub = stub_request(:get, "#{@API_BASE_PATH}/reports/bounces?apikey=#{@DEFAULT_API_KEY}&starttime=#{start_time}&endtime=#{end_time}&startindex=#{start_index}&emailaddress=")
 
         @dyn.bounces.list(start_time,end_time,start_index)
+
+        expect(stub).to have_been_requested
+      end
+
+      it "an email address" do
+        start_time = 1
+        end_time = 2
+        start_index = 40
+        email_address = "abc@domain.com"
+
+        stub = stub_request(:get, "#{@API_BASE_PATH}/reports/bounces?apikey=#{@DEFAULT_API_KEY}&starttime=#{start_time}&endtime=#{end_time}&startindex=#{start_index}&emailaddress=#{email_address}")
+
+        @dyn.bounces.list(start_time,end_time,start_index,email_address)
 
         expect(stub).to have_been_requested
       end
